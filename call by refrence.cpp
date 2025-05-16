60040cfde8233c53277d9bd3abea5b12cf34f28a
rohit13620h <rohit13620h@gmail.com>
2025-05-16 09:35:05 +0545

    syntax error fix

--- call by refrence.cpp
+++ call by refrence.cpp
@@ -1,16 +1,14 @@
 #include<iostream>
 using namespace std;
-
 void increment(int &num);
-
-
 int main(){
 	int num =10;
 	increment(num);
-	cout>>num>>endl;
+cout << num << endl;
 	return 0;
 }
 void increment(int &num){
 	num++;
-	cout>>num>>endl;
+cout << num << endl;
+
 }
